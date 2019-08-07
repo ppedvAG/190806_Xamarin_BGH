32567f34792785442d195256cb9cdd78e24ba0ca
Michael Zöhling <michael.zoehling@outlook.com>
2019-08-07 15:02:18 +0200

    Suche mit der Searchbar und LINQ

--- Listen_Demo/Listen_Demo/Listen_Demo/MainPage.xaml.cs
+++ Listen_Demo/Listen_Demo/Listen_Demo/MainPage.xaml.cs
@@ -64,5 +64,25 @@ namespace Listen_Demo
         {
             // Löschen ;)
         }
+
+        private void SearchBarSuchtext_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            List<Person> personen = new List<Person>
+            {
+                new Person{Vorname="Tom",Nachname="Ate",Alter=10,Kontostand=100},
+                new Person{Vorname="Anna",Nachname="Nass",Alter=20,Kontostand=200},
+                new Person{Vorname="Peter",Nachname="Silie",Alter=30,Kontostand=3000},
+                new Person{Vorname="Franz",Nachname="Ose",Alter=40,Kontostand=-4100},
+                new Person{Vorname="Martha",Nachname="Pfahl",Alter=50,Kontostand=15555500},
+                new Person{Vorname="Albert",Nachname="Tross",Alter=60,Kontostand=12332423},
+                new Person{Vorname="Axel",Nachname="Schweiß",Alter=70,Kontostand=-123100},
+                new Person{Vorname="Rainer",Nachname="Zufall",Alter=80,Kontostand=1909779800},
+                new Person{Vorname="Klara",Nachname="Fall",Alter=90,Kontostand=11212312300},
+                new Person{Vorname="Claire",Nachname="Grube",Alter=100,Kontostand=-12312313100},
+            };
+
+           listViewPersonen.ItemsSource = personen.Where(x => x.Vorname.StartsWith(searchBarSuchtext.Text,StringComparison.InvariantCultureIgnoreCase))
+                                                  .ToList();
+        }
     }
 }
