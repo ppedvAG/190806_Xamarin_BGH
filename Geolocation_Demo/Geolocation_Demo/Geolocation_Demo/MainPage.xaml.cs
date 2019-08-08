963182f37e71f1df79ccdd4683f9600cb717c8fe
Michael Zöhling <michael.zoehling@outlook.com>
2019-08-08 14:46:00 +0200

    Geolocation und Microcharts (anfang)

---
+++ Geolocation_Demo/Geolocation_Demo/Geolocation_Demo/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Geolocation_Demo
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void ButtonLocation_Clicked(object sender, EventArgs e)
+        {
+            var currentLocation = await Geolocation.GetLocationAsync();
+
+            if (currentLocation == null)
+                return;
+
+            labelLocation.Text = $"Latitude: {currentLocation.Latitude}, Longitude: {currentLocation.Longitude}, Altitude: {currentLocation.Altitude}";
+
+            // await currentLocation.OpenMapsAsync();
+
+            var entries = new Microcharts.Entry[]
+            {
+                new Microcharts.Entry(200)
+                {
+                    Label = "January",
+                    ValueLabel = "200",
+                    Color = SKColor.Parse("#266489")
+                },
+                new Microcharts.Entry(400)
+                {
+                    Label = "February",
+                    ValueLabel = "400",
+                    Color = SKColor.Parse("#68B9C0")
+                },
+                new Microcharts.Entry(-100)
+                {
+                    Label = "March",
+                    ValueLabel = "-100",
+                    Color = SKColor.Parse("#90D585")
+                }
+            };
+        }
+    }
+}
