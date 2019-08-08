9897e6f172bb787fbeedad19cc93a434545a671c
Michael Zöhling <michael.zoehling@outlook.com>
2019-08-08 09:45:33 +0200

    ViewModel erstellt

---
+++ MVVM_Simpel/MVVM_Simpel/MVVM_Simpel/ViewModels/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using MVVM_Simpel.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MVVM_Simpel.ViewModels
+{
+    class MainViewModel
+    {
+        public MainViewModel() // ctor + TAB + TAB
+        {
+            PersonenLadenCommand = new Command(PersonenLaden);
+            service = new PersonenService();
+        }
+
+        private readonly PersonenService service; // Geschäftslogik
+
+        private void PersonenLaden(object obj)
+        {
+            Personenliste = service.PersonenLaden();
+        }
+
+        public List<Person> Personenliste { get; set; }
+        public Command PersonenLadenCommand { get; set; }
+
+    }
+}
