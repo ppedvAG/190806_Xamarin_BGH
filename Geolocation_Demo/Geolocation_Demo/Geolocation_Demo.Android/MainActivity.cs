b6a97dd237dd8dc8e8a5c09c06e416c38673afb5
Michael Zöhling <michael.zoehling@outlook.com>
2019-08-08 15:30:04 +0200

    NCF-Berechtigung gesetzt und Code aus folgender Doku eingefügt https://github.com/poz1/NFCForms

--- Geolocation_Demo/Geolocation_Demo/Geolocation_Demo.Android/MainActivity.cs
+++ Geolocation_Demo/Geolocation_Demo/Geolocation_Demo.Android/MainActivity.cs
@@ -6,6 +6,10 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Poz1.NFCForms.Droid;
+using Android.Nfc;
+using Poz1.NFCForms.Abstract;
+using Android.Content;
 
 namespace Geolocation_Demo.Droid
 {
@@ -21,6 +25,14 @@ namespace Geolocation_Demo.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+            //---NFCForms
+            NfcManager NfcManager = (NfcManager)Android.App.Application.Context.GetSystemService(Context.NfcService);
+            NFCdevice = NfcManager.DefaultAdapter;
+
+            Xamarin.Forms.DependencyService.Register<INfcForms, NfcForms>();
+            x = Xamarin.Forms.DependencyService.Get<INfcForms>() as NfcForms;
+            //---
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
@@ -28,6 +40,44 @@ namespace Geolocation_Demo.Droid
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        // NFC-Forms:
+        public NfcAdapter NFCdevice;
+        public NfcForms x;
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            if (NFCdevice != null)
+            {
+                var intent = new Intent(this, GetType()).AddFlags(ActivityFlags.SingleTop);
+                NFCdevice.EnableForegroundDispatch
+                (
+                    this,
+                    PendingIntent.GetActivity(this, 0, intent, 0),
+                    new[] { new IntentFilter(NfcAdapter.ActionTechDiscovered) },
+                    new String[][] {new string[] {
+                            NFCTechs.Ndef,
+                        },
+                        new string[] {
+                            NFCTechs.MifareClassic,
+                        },
+                    }
+                );
+            }
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+            NFCdevice.DisableForegroundDispatch(this);
+        }
+
+        protected override void OnNewIntent(Intent intent)
+        {
+            base.OnNewIntent(intent);
+            x.OnNewIntent(this, intent);
         }
     }
 }
