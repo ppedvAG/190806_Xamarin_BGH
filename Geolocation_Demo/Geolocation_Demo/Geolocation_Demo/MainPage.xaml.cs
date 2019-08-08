5e0cd936c44bfda5cc4038be7506b67b80639e77
Michael Zöhling <michael.zoehling@outlook.com>
2019-08-08 15:59:10 +0200

    Logik für NFC auslesen und schreiben erstellt

--- Geolocation_Demo/Geolocation_Demo/Geolocation_Demo/MainPage.xaml.cs
+++ Geolocation_Demo/Geolocation_Demo/Geolocation_Demo/MainPage.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using Microcharts;
+using NdefLibrary.Ndef;
+using Poz1.NFCForms.Abstract;
 using SkiaSharp;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
@@ -19,7 +22,12 @@ namespace Geolocation_Demo
         public MainPage()
         {
             InitializeComponent();
+            // 1) NFCForms-Service holen
+            nfcService = DependencyService.Get<INfcForms>();
+            // 2) "TagAuslesen" beschreibt, was mit dem gelesenen Tag gemacht werden soll (Zähler, Tag auslesen etc...)
+            nfcService.NewTag += TagAuslesen;
         }
+        INfcForms nfcService;
         Microcharts.Entry[] entries;
 
         private async void ButtonLocation_Clicked(object sender, EventArgs e)
@@ -58,7 +66,6 @@ namespace Geolocation_Demo
             var chart = new LineChart { Entries = entries };
             chartView.Chart = chart;
         }
-
         private void SliderÄndern_ValueChanged(object sender, ValueChangedEventArgs e)
         {
             List<Microcharts.Entry> entries = new List<Microcharts.Entry>();
@@ -94,6 +101,68 @@ namespace Geolocation_Demo
             chart.PointSize = 10;
             chart.PointMode = PointMode.None;
             chartView.Chart = chart;
+        }
+
+        private void TagAuslesen(object sender, NfcFormsTag e)
+        {
+            if (e.IsNdefSupported)
+            {
+               var result = readNDEFMEssage(e.NdefMessage);
+               DisplayAlert("Daten aus dem NFC-Tag", $"{string.Join(";",result)}", "Okay");
+            }
+        }
+
+        private void ButtonWriteNFC_Clicked(object sender, EventArgs e)
+        {
+            var spRecord = new NdefSpRecord
+            {
+                Uri = "www.google.at",
+                NfcAction = NdefSpActRecord.NfcActionType.DoAction,
+            };
+            spRecord.AddTitle(new NdefTextRecord
+            {
+                Text = "NFCForms - XamarinForms - Write-Demo",
+                LanguageCode = "en"
+            });
+            // Add record to NDEF message
+            var msg = new NdefMessage { spRecord };
+            try
+            {
+                nfcService.WriteTag(msg);
+            }
+            catch (Exception excp)
+            {
+                DisplayAlert("Error", excp.Message, "OK");
+            }
+        }
+
+        private IEnumerable<string> readNDEFMEssage(NdefMessage message)
+        {
+            List<string> allTagInfos = new List<string>();
+            if (message == null)
+                return allTagInfos;
+
+            foreach (NdefRecord record in message)
+            {
+                // Go through each record, check if it's a Smart Poster
+                if (record.CheckSpecializedType(false) == typeof(NdefSpRecord))
+                {
+                    // Convert and extract Smart Poster info
+                    var spRecord = new NdefSpRecord(record);
+                    allTagInfos.Add("URI: " + spRecord.Uri);
+                    allTagInfos.Add("Titles: " + spRecord.TitleCount());
+                    allTagInfos.Add("1. Title: " + spRecord.Titles[0].Text);
+                    allTagInfos.Add("Action set: " + spRecord.ActionInUse());
+                }
+
+                if (record.CheckSpecializedType(false) == typeof(NdefUriRecord))
+                {
+                    // Convert and extract Smart Poster info
+                    var spRecord = new NdefUriRecord(record);
+                    allTagInfos.Add("Text: " + spRecord.Uri);
+                }
+            }
+            return allTagInfos;
         }
     }
 }
