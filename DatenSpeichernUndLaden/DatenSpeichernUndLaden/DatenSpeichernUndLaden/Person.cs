5f8376f4c4a26513d1eb9812d0730910154fa065
Michael Zöhling <michael.zoehling@outlook.com>
2019-08-09 10:33:39 +0200

    SQLite

---
+++ DatenSpeichernUndLaden/DatenSpeichernUndLaden/DatenSpeichernUndLaden/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using SQLite;
+
+namespace DatenSpeichernUndLaden
+{
+    [Table("Personentabelle")]
+    class Person
+    {
+        [PrimaryKey,AutoIncrement]
+        public int ID { get; set; }
+        [MaxLength(255)]
+        public string Name { get; set; }
+        public decimal Kontostand { get; set; }
+    }
+}
---
+++ DatenSpeichernUndLaden/DatenSpeichernUndLaden/DatenSpeichernUndLaden/SQLitePage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DatenSpeichernUndLaden
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SQLitePage : ContentPage
+    {
+        public SQLitePage()
+        {
+            InitializeComponent();
+
+            string fullpath = Path.Combine(FileSystem.AppDataDirectory, "demo.sqlite");
+            con = new SQLiteConnection(fullpath);
+            // Wenn die Datei bereits existiert, wird sie geöffnet. Ansonsten wird sie neu erstellt
+
+            // Beim ersten Start:
+            con.CreateTable<Person>(); // Praktisch: Wenn die Tabelle bereits existiert, passiert nichts
+        }
+
+        private SQLiteConnection con;
+
+        private void ButtonEinfügen_Clicked(object sender, EventArgs e)
+        {
+            Person p1 = new Person();
+            p1.Name = entryName.Text;
+            p1.Kontostand = Convert.ToDecimal(entryKontostand.Text);
+
+            con.Insert(p1);
+        }
+
+        private void ListViewPersonen_Refreshing(object sender, EventArgs e)
+        {
+            listViewPersonen.ItemsSource = con.Table<Person>().ToList();
+            listViewPersonen.EndRefresh();
+        }
+    }
+}
