0fe74e911104b9f09bd45b75eeba562821c8b553
Michael Zöhling <michael.zoehling@outlook.com>
2019-08-09 09:28:59 +0200

    Settings

--- DatenSpeichernUndLaden/DatenSpeichernUndLaden/DatenSpeichernUndLaden/MainPage.xaml.cs
+++ DatenSpeichernUndLaden/DatenSpeichernUndLaden/DatenSpeichernUndLaden/MainPage.xaml.cs
@@ -11,7 +11,7 @@ namespace DatenSpeichernUndLaden
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
-    public partial class MainPage : ContentPage
+    public partial class MainPage : Shell
     {
         public MainPage()
         {
---
+++ DatenSpeichernUndLaden/DatenSpeichernUndLaden/DatenSpeichernUndLaden/SettingsPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DatenSpeichernUndLaden
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SettingsPage : ContentPage
+    {
+        public SettingsPage()
+        {
+            InitializeComponent();
+            entryUsername.Text = Preferences.Get("Username", "--- Kein Username gespeichert ---");
+            switchNotifications.IsToggled = Preferences.Get("Notifications", false);
+            sliderWert.Value = Preferences.Get("Wert", 0.0);
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Preferences.Set("Username", entryUsername.Text);
+            Preferences.Set("Notifications", switchNotifications.IsToggled);
+            Preferences.Set("Wert", sliderWert.Value);
+        }
+    }
+}
