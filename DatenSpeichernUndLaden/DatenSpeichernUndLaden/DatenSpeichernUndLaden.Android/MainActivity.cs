6fed05ff83b50a028b304bc84be3c0f37f235ab5
Michael Zöhling <michael.zoehling@outlook.com>
2019-08-09 09:45:53 +0200

    Textdokument

--- DatenSpeichernUndLaden/DatenSpeichernUndLaden/DatenSpeichernUndLaden.Android/MainActivity.cs
+++ DatenSpeichernUndLaden/DatenSpeichernUndLaden/DatenSpeichernUndLaden.Android/MainActivity.cs
@@ -22,6 +22,9 @@ namespace DatenSpeichernUndLaden.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+
+            // Ordner in Android:
+            // Android.OS.Environment.DirectoryMusic
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
---
+++ DatenSpeichernUndLaden/DatenSpeichernUndLaden/DatenSpeichernUndLaden/TextPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DatenSpeichernUndLaden
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TextPage : ContentPage
+    {
+        public TextPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonSpeichern_Clicked(object sender, EventArgs e)
+        {
+            // Problem: Ort schaut überall anders aus
+            // Android: SD-Karte: /storage/emulated/0/Documents
+            // Windows(UWP): C:/Users/Michael/AppData/Local
+            // usw...
+
+            string fullPath = Path.Combine(FileSystem.AppDataDirectory, "demo.txt");
+            File.WriteAllText(fullPath, entryInhalt.Text);
+        }
+
+        private void ButtonLaden_Clicked(object sender, EventArgs e)
+        {
+            string fullPath = Path.Combine(FileSystem.AppDataDirectory, "demo.txt");
+            DisplayAlert(fullPath, File.ReadAllText(fullPath), "Ok");
+        }
+    }
+}
