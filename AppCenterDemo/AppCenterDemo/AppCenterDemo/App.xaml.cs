8d893e6e09f8af60caf80e6319b031d54acf1093
Michael Zöhling <michael.zoehling@outlook.com>
2019-08-09 11:21:54 +0200

    CrashApp mit VisualStudio App Center

---
+++ AppCenterDemo/AppCenterDemo/AppCenterDemo/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppCenterDemo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            AppCenter.Start("android=16b6b263-620b-4c4a-a8e2-082a9608dab1;" ,typeof(Analytics), typeof(Crashes));
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ AppCenterDemo/AppCenterDemo/AppCenterDemo/MainPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AppCenter.Crashes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppCenterDemo
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : Shell
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void DivideByZero_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                throw new DivideByZeroException();
+            }
+            catch (Exception ex)
+            {
+                Crashes.TrackError(ex);
+                // normales error-handling
+            }
+        }
+
+        private void StackOverflow_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                throw new StackOverflowException();
+            }
+            catch (Exception ex)
+            {
+                Crashes.TrackError(ex);
+                // normales error-handling
+            }
+        }
+
+        private void Crash_Clicked(object sender, EventArgs e)
+        {
+            IchMacheEtwasDummes();
+        }
+        private void IchMacheEtwasDummes()
+        {
+            EsGibtEinenCrash();
+        }
+        private void EsGibtEinenCrash()
+        {
+            Crashes.GenerateTestCrash();
+        }
+    }
+}
